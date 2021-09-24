6fff88e0fad20a5a6de514eece785c579a1cd3f1
Monica Cruz <mcatcruz@gmail.com>
2021-09-24 13:59:06 -0400

    Added unit tests for student.py

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,6 +1,20 @@
 from school_schedule.student import Student
+
 def test_student_class_name_is_correct():
   # act
   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
   # assert
   assert student.name == "Trenisha"
+
+def test_add_class_adds_class():
+    # arrange
+    student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+
+    adds_class = student.classes.append("Ceramics")
+
+    assert student.classes == ["Calculus", "Choir", "Photography", "AP History", "Ceramics"]
+
+def test_class_list_length_is_correct():
+    student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+    list_length = student.get_num_classes()
+    assert list_length == 4
